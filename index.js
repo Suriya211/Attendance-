8f45557697df3602ff89addfd194c45103f7ad9b
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-09-23 13:59:32 +0000

    add function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function Password2Name(){
   document.getElementById('inputPassword2').value.toLowerCase();
 let name = document.getElementById('name');
 }
+console.log(name);
 surname:{
   let inputPassword2 =
   document.getElementById('inputPassword2').value.toLowerCase();
@@ -13,7 +14,7 @@ phone:{
   document.getElementById('inputPassword2').value.toLowerCase();
 let name = document.getElementById('phone number');
 };
-
+console.log(phoneNumber);
 email:{
   let inputPassword2 =
   document.getElementById('inputPassword2').value.toLowerCase();
@@ -24,66 +25,10 @@ address:{
   document.getElementById('inputPassword2').value.toLowerCase();
 let name = document.getElementById('address');
 }
+console.log(address);
 date:{
   let inputPassword2 =
   document.getElementById('inputPassword2').value.toLowerCase();
 let name = document.getElementById('date');
 }
-
-
-function doGet(e){
-  return handleResponse(e);
-}
-
-        var SHEET_NAME = "Sheet1";
-        
-
-
-var SCRIPT_PROP = PropertiesService.getScriptProperties();
-
-
-function doPost(e){
-  return handleResponse(e);
-}
-
-function handleResponse(e) {
- 
-  var lock = LockService.getPublicLock();
-  lock.waitLock(30000);  
-  
-  try {
-
-    var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
-    var sheet = doc.getSheetByName(SHEET_NAME);
-    
-    var headRow = e.parameter.header_row || 1;
-    var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-    var nextRow = sheet.getLastRow()+1;
-    var row = []; 
-    
-    for (i in headers){
-      if (headers[i] == "Timestamp"){ 
-      } else { 
-        row.push(e.parameter[headers[i]]);
-      }
-    }
-    
-    sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);
-   
-    return ContentService
-          .createTextOutput(JSON.stringify({"result":"success", "row": nextRow}))
-          .setMimeType(ContentService.MimeType.JSON);
-  } catch(e){
-    
-    return ContentService
-          .createTextOutput(JSON.stringify({"result":"error", "error": e}))
-          .setMimeType(ContentService.MimeType.JSON);
-  } finally { 
-    lock.releaseLock();
-  }
-}
-
-function setup() {
-    var doc = SpreadsheetApp.getActiveSpreadsheet();
-    SCRIPT_PROP.setProperty("key", doc.getId());
-}
+console.log(Date);
